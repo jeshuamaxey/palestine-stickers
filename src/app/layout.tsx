81b9a517edde44b4c8e0ba0089d90d1a6b462885
Jeshua Maxey <me@jeshua.co>
2024-07-25 17:05:50 +0100

    fruitless favicon tweak

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,16 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Head from "next/head";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Palestine stickers",
   description: "Stickers expressing solidarity with the Palestinian cause. All profits go to Medical Aid to Palestinians.",
+  icons: {
+    icon: "/favicon.ico",
+  }
 };
 
 export default function RootLayout({
@@ -16,9 +20,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <head>
+      <Head>
         <link rel="icon" href="/favicon.ico" sizes="any" />
-      </head>
+      </Head>
       <body className={inter.className}>{children}</body>
     </html>
   );
